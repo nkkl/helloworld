331567aa1ed6128b2ee85dae3d99c56325a5ff47
Nikki Lee <nikki@nicoleblee.com>
2012-07-13 16:25:57 -0700

    first commit, setting up rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helloworld::Application.config.secret_token = 'baaf0c51159976420d6c8b646dd7eefb474e98e28b258665f0f07737ed5586e413a62db56c2b24fcc1de78a09bda01a363ebf3b81fd56672ac3a3f76dfc0c151'
